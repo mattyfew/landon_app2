c9d29f67a2e358d760d75f9c02922df341c62803
Matt Fewer <mattfewer@hotmail.com>
2018-09-03 16:53:17 +0200

    course complete. should start Laravel Essentials 2 next

--- app/Http/Controllers/ReservationsController.php
+++ app/Http/Controllers/ReservationsController.php
@@ -1,14 +1,34 @@
 <?php
-
 namespace App\Http\Controllers;
+use App\Client as Client;
+use App\Room as Room;
+use App\Reservation as Reservation;
 
 use Illuminate\Http\Request;
 
 class ReservationsController extends Controller
 {
     //
-    public function bookRoom()
+    public function bookRoom($client_id, $room_id, $date_in, $date_out)
     {
-        return view('reservations/bookRoom');
+        $reservation = new Reservation();
+        $client_instance = new Client();
+        $room_instance = new Room();
+
+        $client = $client_instance->find($client_id);
+        $room = $room_instance->find($room_id);
+        $reservation->date_in = $date_in;
+        $reservation->date_out = $date_out;
+
+        $reservation->room()->associate($room);
+        $reservation->client()->associate($client);
+        if ($room_instance->isRoomBooked($room_id, $date_in, $date_out))
+        {
+            abort(405, 'Trying to book and already booked room');
+        }
+        $reservation->save();
+
+        return redirect()->route('clients');
+        // return view('reservations/bookRoom');
     }
 }
